f278fc4976f144e7ed88bd7c8b577cc7a6444b96
boggoriss <boriskust01@gmail.com>
2022-11-12 22:07:45 +0300

    add_models: сваггер

--- Diner/Program.cs
+++ Diner/Program.cs
@@ -1,6 +1,8 @@
+using System.Reflection;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ServicesLib;
 using UtilsLib.Configurations;
 
@@ -28,20 +30,20 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddAuthorization();
 
 builder.Services.Configure<DbConfig>(builder.Configuration.GetSection("DbConfig"));
-
 builder.Services.AddServicesLib(builder.Configuration);
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo() { Title = "Diner API", Version = "v1" });
+});
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Diner API");
+});
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-app.UseSwagger();
-app.UseSwaggerUI((options) =>
-{
-    options.RoutePrefix = string.Empty;
-    options.SwaggerEndpoint("/api", "api");
-});
 app.Run();
