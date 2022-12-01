0a78c5ed64876a99bb908e4fb48bb3fc1de60911
Quuuuuuux <a.v.shunaev@gmail.com>
2022-12-01 11:43:32 +0300

    test data

--- Diner/Program.cs
+++ Diner/Program.cs
@@ -61,9 +61,72 @@ app.UseExceptionHandler(exceptionHandlerApp => {
 });
 
 var userService = app.Services.GetService<UserService>();
+var dishService = app.Services.GetService<DishService>();
+var paymentService = app.Services.GetService<PaymentService>();
+var resourceService = app.Services.GetService<ResourceService>();
+
 var users = await userService.FindAllAsync();
+var dishes = await dishService.FindAllAsync();
+var payments = await paymentService.FindAllAsync();
+var resources = await resourceService.FindAllAsync();
+User user = new User();
 if (users.Count == 0)
 {
-    await userService.CreateUserWithDefaults(new CreateUserDto() { FullName = "Test Test", Login = "test", Role = UserRole.Admin, Password = "test-test"});
+    user = await userService.CreateUserWithDefaults(new CreateUserDto() { FullName = "Test Test", Login = "test", Role = UserRole.Admin, Password = "test-test"});
 }
+
+if (dishes.Count == 0)
+{
+    await dishService.CreateDish(new DishDto()
+    {
+        Name = "Onion Soup",
+        Description = "Soup from onions",
+        DishType = DishType.Soup,
+        Price = 15,
+    });
+}
+
+if (payments.Count == 0)
+{
+    await paymentService.CreateDefaultPayment(new PaymentDto()
+    {
+        Description = "VERY GOOD",
+        Price = 100,
+        Number = 0,
+        Status = PaymentStatus.Approval,
+        Type = PaymentType.Lease,
+        UserId = user.Id,
+    });
+    await paymentService.CreateDefaultPayment(new PaymentDto()
+    {
+        Description = "SUPER",
+        Price = 200,
+        Number = 0,
+        Status = PaymentStatus.Approval,
+        Type = PaymentType.Lease,
+        UserId = user.Id,
+    });
+    await paymentService.CreateDefaultPayment(new PaymentDto()
+    {
+        Description = "VERY FINE",
+        Price = 500,
+        Number = 0,
+        Status = PaymentStatus.Approval,
+        Type = PaymentType.Lease,
+        UserId = user.Id,
+    });
+    
+}
+
+if (resources.Count == 0)
+{
+    await resourceService.CreateResource(new ResourceDto()
+    {
+        Amount = 1000,
+        Name = "Onion",
+        Unit = Unit.Kg,
+    });
+}
+
+
 app.Run();
